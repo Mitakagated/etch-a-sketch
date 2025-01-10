94e8e2fdefd85b358d91b62ddf4ee9ebe2a7387b
Dimitar Petrov <dimitarpetrov44@gmail.com>
2025-01-10 15:58:00 +0200

    Add a white trail on the squares behind the cursor

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ square.classList.add('square');
 for (let i = 0; i < 16; i++) {
     for (let index = 0; index < 16; index++) {
         container.appendChild(square.cloneNode(true));
-        console.log(square);
     }
 }
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'white';
+    })
+})
